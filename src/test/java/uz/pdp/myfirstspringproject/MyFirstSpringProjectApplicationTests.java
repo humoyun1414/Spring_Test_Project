03d4f9f01a075f25d57695b88245df75881914d8
Humoyun <humoyunabdurahmonov49@gmail.com>
2022-04-23 17:39:38 +0500

    My First Commit

---
+++ src/test/java/uz/pdp/myfirstspringproject/MyFirstSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.myfirstspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstSpringProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
